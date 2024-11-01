0d7c9cec12269be0ad04f69da08ae022668e992a
kooshaan <kooshan90@gmail.com>
2024-11-01 17:58:57 +0330

    a

--- app/src/main/java/com/example/nokiatest/etc/ArtSpaceApp.kt
+++ app/src/main/java/com/example/nokiatest/etc/ArtSpaceApp.kt
@@ -10,11 +10,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,12 +32,16 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.nokiatest.JetTipApp.R
 
 class ArtSpaceApp : ComponentActivity() {
@@ -54,24 +62,24 @@ fun App(){
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun BaseCanvas(shownID: Int = 0,
-               onValChange: (Int) -> Unit = {}){
+fun BaseCanvas(shownID: Int,
+               onValChange: (Int) -> Unit){
     val bruh: Array<Pair<Float, Color>> = arrayOf(
         0.0f to Color(red = 33, green = 39, blue = 63, alpha = 255),
-        1.0f to Color(red = 217, green = 245, blue = 245, alpha = 255)
+        1.0f to Color(red = 251, green = 214, blue = 255, alpha = 255)
+    )
+    val buttonsColor = ButtonDefaults.buttonColors(
+        Color(red = 105, green = 86, blue = 142, alpha = 255)
     )
     Box(modifier = Modifier
+        .fillMaxSize()
         .clip(RoundedCornerShape(CornerSize(8.dp)))
         .background(
-            brush = Brush.linearGradient(
-                colorStops = bruh,
-                start = Offset(Float.MAX_VALUE, 0.0f),
-                end = Offset(0.0f, Float.MAX_VALUE)
+            brush = Brush.verticalGradient(
+                colorStops = bruh
             )
-        )
-        .wrapContentSize(align = Alignment.Center, true),
+        ),
         contentAlignment = Alignment.TopCenter
     ){
         Surface(
@@ -83,47 +91,80 @@ fun BaseCanvas(shownID: Int = 0,
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Surface(
                     modifier = Modifier
+                        .requiredWidth(300.dp)
+                        .requiredHeight(460.dp)
                         .clip(RoundedCornerShape(CornerSize(6.dp)))
-                        .padding(vertical = 8.dp, horizontal = 8.dp),
+                        .padding(top = 24.dp, bottom = 12.dp, start = 8.dp, end = 8.dp),
                     shadowElevation = 8.dp,
                     color = Color.Transparent
                 ) {
                     Image(painter = painterResource(id = R.drawable.avatar),
-                        contentDescription = "123")
+                        contentDescription = "123",
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier.padding(32.dp)
+                        )
                 }
-                Surface(color = Color.Transparent) {
+                Surface(
+                    modifier = Modifier
+                        .padding(top = 8.dp, bottom = 24.dp)
+                        .clip(RoundedCornerShape(CornerSize(8.dp))),
+                    color = Color(red = 63, green = 49, blue = 83, alpha = 156)) {
                     Column(verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(text = "artwork title")
+
+                        Text(text = "artwork title",
+                            fontSize = 28.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.LightGray)
+
                         Row {
                             Text(text = buildAnnotatedString {
-                                withStyle(SpanStyle()) {
+                                withStyle(SpanStyle(
+                                    color = Color.LightGray,
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.SemiBold
+                                )) {
                                     append("artist name")
                                 }
-                            })
+                            }, modifier = Modifier.padding(8.dp))
+
                             Text(text = buildAnnotatedString {
-                                withStyle(SpanStyle()) {
-                                    append("year created")
+                                withStyle(SpanStyle(
+                                    color = Color.LightGray,
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Normal,
+                                    fontStyle = FontStyle.Italic,
+                                    shadow = Shadow(Color.DarkGray, Offset(0.05f, 0.05f))
+                                )) {
+                                    append("\tyear created")
                                 }
-                            })
+                            }, modifier = Modifier.padding(8.dp))
                         }
                     }
                 }
-                Surface {
+                Surface(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = Color.Transparent
+                ) {
                     Row(verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.Center) {
+
                         Button(
                             onClick = { onValChange(shownID - 1)},
-                            modifier = Modifier.weight(1.0f, false)
+                            modifier = Modifier.weight(1.0f, false),
+                            colors = buttonsColor
                             ) {
                             Text(text = "Previous artwork")
                         }
-                        Spacer(modifier = Modifier.weight(4.0f, true))
+
+                        Spacer(modifier = Modifier.weight(1.0f, true))
+
                         Button(
                             onClick = { onValChange(shownID + 1)},
-                            modifier = Modifier.weight(1.0f, false)
+                            modifier = Modifier.weight(1.0f, false),
+                            colors = buttonsColor
                             ) {
-                            Text(text = "Next creation")
+                            Text(text = "Next creation", )
                         }
                     }
                 }
---
+++ app/src/test/java/com/example/nokiatest/jettipapp/JetTipAppTests.kt
@@ -0,0 +1,3 @@
+package com.example.nokiatest.jettipapp
+
+class JetTipAppTests
