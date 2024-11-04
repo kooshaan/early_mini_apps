2dc26c264c154813b2f41cea64c726af56eb5722
kooshaan <kooshan90@gmail.com>
2024-11-05 01:27:30 +0330

    Material Design

--- app/src/main/java/com/example/nokiatest/movieapp/MovieApp.kt
+++ app/src/main/java/com/example/nokiatest/movieapp/MovieApp.kt
@@ -1,5 +1,3 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.example.nokiatest.movieapp
 
 import android.annotation.SuppressLint
@@ -7,24 +5,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.nokiatest.movieapp.navigation.MovieNavigation
+import com.example.nokiatest.ui.theme.NokiaTestTheme
 
 class MovieApp : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApp {
-                MovieNavigation()
+            NokiaTestTheme {
+                MyApp {
+                    MovieNavigation()
+                }
             }
         }
     }
 }
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MyApp(content: @Composable () -> Unit){
+fun MyApp(modifier: Modifier = Modifier,
+          content: @Composable () -> Unit){
         content()
+}
 
+@Preview
+@Composable
+fun MovieAppDarkThemePreview(){
+    NokiaTestTheme(darkTheme = true) {
+        MyApp {
+            MovieNavigation()
+        }
+    }
 }
